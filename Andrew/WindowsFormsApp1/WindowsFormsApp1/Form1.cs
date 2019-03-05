400c06b4c1db7f426e292ecec0062d8d71f8eb43
ComputerScientistAndrew <44506286+ComputerScientistAndrew@users.noreply.github.com>
2019-03-05 00:17:38 -0600

    C# GUI in Visual Studio & To do list
    -Potential GUI in Visual Studio using C#
    -Added To do list but is not completed

---
+++ Andrew/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Login : Form
+    {
+        //StreamReader sr = new StreamReader("file.txt");
+        //StreamWriter sw = new StreamWriter(@"C:\");
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Hi, " + textBox1.Text);
+            Form f = new Form2();
+            f.Show();
+
+            this.Hide();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
