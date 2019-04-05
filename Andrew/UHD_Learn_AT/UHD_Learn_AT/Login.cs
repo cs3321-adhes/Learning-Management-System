8d94c40c79e42432cabaed307e4cf8017619e1f6
ComputerScientistAndrew <44506286+ComputerScientistAndrew@users.noreply.github.com>
2019-04-05 00:43:26 -0500

    Added comments to Login scripts

--- Andrew/UHD_Learn_AT/UHD_Learn_AT/Login.cs
+++ Andrew/UHD_Learn_AT/UHD_Learn_AT/Login.cs
@@ -13,6 +13,7 @@ namespace UHD_Learn_AT
 {
     public partial class Login : Form
     {
+        // Declaration of Sql connection.  The input is from "connection string" of the database properties.
         SqlConnection con = new SqlConnection("Data Source=DESKTOP-ANDREW\\SQLEXPRESS;Initial Catalog=UHD_LEARN;Integrated Security=True");
 
         public Login()
@@ -22,30 +23,52 @@ namespace UHD_Learn_AT
 
         private void loginButton_Click(object sender, EventArgs e)
         {
+            // opens connection
             con.Open();
+            // Sql command to find the row that matches the username and password the user inputs.
             SqlCommand cmd = new SqlCommand("SELECT * FROM Login where username='" + userBox.Text + "' and password='" + pwBox.Text + "'", con);
+
+            // This is the sql data reader to read the data.
             SqlDataReader rd = cmd.ExecuteReader();
+
+            // this is the initialization of varables to store info of the user to use later
             string logType = "";
             string ID = "";
             string fname = "";
             string lname = "";
+
+            // if there is a row(meaning if the username and password matches)
             if (rd.HasRows)
             {
+                // If the reader is able to read
                 if (rd.Read())
                 {
+                    // This gets the string in the index of the columns.  We need to trim to get rid of the empty spaces
                     fname = rd.GetString(2).Trim();
                     lname = rd.GetString(3).Trim();
                     ID = rd.GetString(4).Trim();
                     logType = rd.GetString(5).Trim();
                 }
+
+                // closes reader
                 rd.Close();
                 
+                // If the type of user matches teacher
                 if (logType.Equals("teacher"))
                 {
+                    // This statement creates a new teacherpage form object
                     TeacherPage teacherpage = new TeacherPage();
+
+                    // This statement sets the top of the window text to hello lname
                     teacherpage.Text = "Welcome, " + lname;
+
+                    // This calls a method from teacherpage that changes the label to the first and last name of the user
                     teacherpage.ChangeLabel(fname + " " + lname); 
+
+                    // this pops open the teacher page form
                     teacherpage.Show();
+
+                    // this hides this form window
                     this.Hide();
                 }
                 else if (logType.Equals("student"))
@@ -57,8 +80,11 @@ namespace UHD_Learn_AT
             }
             else
             {
+                // if username and password doesn't match, a message box will open and display this message
                 MessageBox.Show("Invalid Username and Password.");
             }
+
+            // closes DB connection
             con.Close();
         }
     }
