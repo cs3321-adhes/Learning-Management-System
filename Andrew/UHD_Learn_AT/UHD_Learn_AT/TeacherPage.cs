b512d58e3fbc8e1c29db8a8452d797d792064243
Andrew Truong <andrewtruong829@yahoo.com>
2019-04-06 17:18:26 -0500

    -Found solution to issue with replacing values in list

--- Andrew/UHD_Learn_AT/UHD_Learn_AT/TeacherPage.cs
+++ Andrew/UHD_Learn_AT/UHD_Learn_AT/TeacherPage.cs
@@ -13,9 +13,9 @@ namespace UHD_Learn_AT
 {
     public partial class TeacherPage : Form
     {
-        SqlConnection con = new SqlConnection("Data Source=DESKTOP-ANDREW\\SQLEXPRESS;Initial Catalog=UHD_LEARN;Integrated Security=True");
+        //SqlConnection con = new SqlConnection("Data Source=DESKTOP-ANDREW\\SQLEXPRESS;Initial Catalog=UHD_LEARN;Integrated Security=True");
 
-        //SqlConnection con = new SqlConnection("Data Source=ASUSVIVOBOOK\\SQLEXPRESS;Initial Catalog=UHD_Learn;Integrated Security=True");
+        SqlConnection con = new SqlConnection("Data Source=ASUSVIVOBOOK\\SQLEXPRESS;Initial Catalog=UHD_Learn;Integrated Security=True");
         
 
         public void ChangeLabel(string s)
@@ -33,7 +33,7 @@ namespace UHD_Learn_AT
         {
             
             List<string[]> courseNames = new List<string[]>();
-            string[] course = new string[4];
+            string[] course;
             con.Open();
             SqlCommand cmd = new SqlCommand("SELECT * FROM Course where teacher='" + label1.Text + "'", con);
             SqlDataReader rd = cmd.ExecuteReader();
@@ -42,6 +42,7 @@ namespace UHD_Learn_AT
             {
                 while (rd.Read())
                 {
+                    course = new string[4];
                     for (int i = 0; i < 4; i++)
                     {
                         course[i] = rd.GetString(i).Trim();
@@ -54,7 +55,7 @@ namespace UHD_Learn_AT
                 }
             }
 
-            courseLabel1.Text = courseNames[0][0] + " " + courseNames[0][1] + "_" + courseNames[0][2] + ": " + courseNames[0][3];
+           // courseLabel1.Text = courseNames[0][0] + " " + courseNames[0][1] + "_" + courseNames[0][2] + ": " + courseNames[0][3];
             
 
         }
