08f3bd17948366d4df9ce480730ab32c5718fe9d
Eduardo Rojas <43352182+eddyguy22@users.noreply.github.com>
2019-04-18 12:38:36 -0500

    CoursePAge

---
+++ Eduardo/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.AssignmentBtt = new System.Windows.Forms.Button();
+            this.CourseConBtt = new System.Windows.Forms.Button();
+            this.SyllabusBtt = new System.Windows.Forms.Button();
+            this.GradeBtt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.HomeBtt = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LogOutBtt = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.HomePanel = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.SyllabusPanel = new System.Windows.Forms.Panel();
+            this.SyllaLabel = new System.Windows.Forms.LinkLabel();
+            this.CourseConPanel = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.HomePanel.SuspendLayout();
+            this.SyllabusPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.AssignmentBtt);
+            this.panel1.Controls.Add(this.CourseConBtt);
+            this.panel1.Controls.Add(this.SyllabusBtt);
+            this.panel1.Controls.Add(this.GradeBtt);
+            this.panel1.Location = new System.Drawing.Point(12, 188);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(158, 360);
+            this.panel1.TabIndex = 0;
+            // 
+            // AssignmentBtt
+            // 
+            this.AssignmentBtt.Location = new System.Drawing.Point(14, 138);
+            this.AssignmentBtt.Name = "AssignmentBtt";
+            this.AssignmentBtt.Size = new System.Drawing.Size(126, 30);
+            this.AssignmentBtt.TabIndex = 2;
+            this.AssignmentBtt.Text = "Assignments";
+            this.AssignmentBtt.UseVisualStyleBackColor = true;
+            this.AssignmentBtt.Click += new System.EventHandler(this.AssignmentBtt_Click);
+            // 
+            // CourseConBtt
+            // 
+            this.CourseConBtt.Location = new System.Drawing.Point(14, 102);
+            this.CourseConBtt.Name = "CourseConBtt";
+            this.CourseConBtt.Size = new System.Drawing.Size(126, 30);
+            this.CourseConBtt.TabIndex = 1;
+            this.CourseConBtt.Text = "Course Content";
+            this.CourseConBtt.UseVisualStyleBackColor = true;
+            this.CourseConBtt.Click += new System.EventHandler(this.CourseConBtt_Click);
+            // 
+            // SyllabusBtt
+            // 
+            this.SyllabusBtt.Location = new System.Drawing.Point(14, 66);
+            this.SyllabusBtt.Name = "SyllabusBtt";
+            this.SyllabusBtt.Size = new System.Drawing.Size(126, 30);
+            this.SyllabusBtt.TabIndex = 0;
+            this.SyllabusBtt.Text = "Syllabus";
+            this.SyllabusBtt.UseVisualStyleBackColor = true;
+            this.SyllabusBtt.Click += new System.EventHandler(this.SyllabusBtt_Click);
+            // 
+            // GradeBtt
+            // 
+            this.GradeBtt.Location = new System.Drawing.Point(14, 174);
+            this.GradeBtt.Name = "GradeBtt";
+            this.GradeBtt.Size = new System.Drawing.Size(126, 30);
+            this.GradeBtt.TabIndex = 2;
+            this.GradeBtt.Text = "My Grades";
+            this.GradeBtt.UseVisualStyleBackColor = true;
+            this.GradeBtt.Click += new System.EventHandler(this.GradeBtt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(141, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Home Page";
+            // 
+            // HomeBtt
+            // 
+            this.HomeBtt.Location = new System.Drawing.Point(26, 36);
+            this.HomeBtt.Name = "HomeBtt";
+            this.HomeBtt.Size = new System.Drawing.Size(90, 40);
+            this.HomeBtt.TabIndex = 4;
+            this.HomeBtt.Text = "Home Page";
+            this.HomeBtt.UseVisualStyleBackColor = true;
+            this.HomeBtt.Click += new System.EventHandler(this.HomeBtt_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(715, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Eduardo Rojas";
+            // 
+            // LogOutBtt
+            // 
+            this.LogOutBtt.Location = new System.Drawing.Point(910, 36);
+            this.LogOutBtt.Name = "LogOutBtt";
+            this.LogOutBtt.Size = new System.Drawing.Size(75, 40);
+            this.LogOutBtt.TabIndex = 6;
+            this.LogOutBtt.Text = "Log Out";
+            this.LogOutBtt.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Course Main";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.richTextBox1);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(11, 105);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(973, 65);
+            this.panel2.TabIndex = 9;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(224, 25);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(229, 24);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // HomePanel
+            // 
+            this.HomePanel.BackColor = System.Drawing.SystemColors.Window;
+            this.HomePanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.HomePanel.Controls.Add(this.label5);
+            this.HomePanel.Controls.Add(this.textBox1);
+            this.HomePanel.Controls.Add(this.label4);
+            this.HomePanel.Controls.Add(this.monthCalendar1);
+            this.HomePanel.Location = new System.Drawing.Point(235, 188);
+            this.HomePanel.Name = "HomePanel";
+            this.HomePanel.Size = new System.Drawing.Size(749, 359);
+            this.HomePanel.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(40, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 17);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Quick Notes";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBox1.Location = new System.Drawing.Point(43, 66);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(326, 162);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(451, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Calendar";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.monthCalendar1.Location = new System.Drawing.Point(454, 66);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            // 
+            // SyllabusPanel
+            // 
+            this.SyllabusPanel.BackColor = System.Drawing.SystemColors.Window;
+            this.SyllabusPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SyllabusPanel.Controls.Add(this.SyllaLabel);
+            this.SyllabusPanel.Location = new System.Drawing.Point(235, 188);
+            this.SyllabusPanel.Name = "SyllabusPanel";
+            this.SyllabusPanel.Size = new System.Drawing.Size(229, 359);
+            this.SyllabusPanel.TabIndex = 11;
+            this.SyllabusPanel.Visible = false;
+            // 
+            // SyllaLabel
+            // 
+            this.SyllaLabel.AutoSize = true;
+            this.SyllaLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SyllaLabel.Location = new System.Drawing.Point(58, 50);
+            this.SyllaLabel.Name = "SyllaLabel";
+            this.SyllaLabel.Size = new System.Drawing.Size(111, 20);
+            this.SyllaLabel.TabIndex = 0;
+            this.SyllaLabel.TabStop = true;
+            this.SyllaLabel.Text = "Class Syllabus";
+            // 
+            // CourseConPanel
+            // 
+            this.CourseConPanel.BackColor = System.Drawing.SystemColors.Window;
+            this.CourseConPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CourseConPanel.Location = new System.Drawing.Point(235, 187);
+            this.CourseConPanel.Name = "CourseConPanel";
+            this.CourseConPanel.Size = new System.Drawing.Size(748, 359);
+            this.CourseConPanel.TabIndex = 12;
+            this.CourseConPanel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 590);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.LogOutBtt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.HomeBtt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.HomePanel);
+            this.Controls.Add(this.CourseConPanel);
+            this.Controls.Add(this.SyllabusPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.HomePanel.ResumeLayout(false);
+            this.HomePanel.PerformLayout();
+            this.SyllabusPanel.ResumeLayout(false);
+            this.SyllabusPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button AssignmentBtt;
+        private System.Windows.Forms.Button CourseConBtt;
+        private System.Windows.Forms.Button SyllabusBtt;
+        private System.Windows.Forms.Button GradeBtt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button HomeBtt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button LogOutBtt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel HomePanel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel SyllabusPanel;
+        private System.Windows.Forms.LinkLabel SyllaLabel;
+        private System.Windows.Forms.Panel CourseConPanel;
+    }
+}
+
